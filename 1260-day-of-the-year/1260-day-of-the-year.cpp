79274d2b1c1be7452005f47fef3dcb1261d1324e
Aaryan Gupta <aaryang0108@gmail.com>
2026-07-30 23:50:50 +0530

    Time: 0 ms (100%), Space: 12.3 MB (26.36%) - LeetHub

---
+++ 1260-day-of-the-year/1260-day-of-the-year.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dayOfYear(string date) {
+        int year = stoi(date.substr(0,4));
+        int month = stoi(date.substr(5,2));
+        int dat = stoi(date.substr(8,2));
+
+        int months[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+        if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+            months[1] = 29;
+        }
+
+        for(int i=0;i<month-1;i++){
+            dat += months[i];
+        }
+        return dat;
+    }
+};
