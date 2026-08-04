bc6420f435455ecc5010138f8c92d4273a008cf7
Aaryan Gupta <aaryang0108@gmail.com>
2026-08-04 23:56:32 +0530

    Time: 0 ms (100%), Space: 32.3 MB (54.61%) - LeetHub

--- 4107-find-missing-elements/4107-find-missing-elements.cpp
+++ 4107-find-missing-elements/4107-find-missing-elements.cpp
@@ -3,8 +3,7 @@ public:
     vector<int> findMissingElements(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<int> miss;
-        int n = nums.size();
-        for(int i=nums[0];i<=nums[n-1];i++){
+        for(int i=nums[0];i<=nums[nums.size()-1];i++){
             if(!binary_search(nums.begin(),nums.end(),i)){
                 miss.push_back(i);
             }
